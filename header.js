8ed45b4a54e7578345f06fefb845ad75003f4239
Katie Park <katiepark@Katies-MBP.attlocal.net>
2019-11-18 17:22:05 -0500

    gallery setup (pre-animation)

--- header.js
+++ header.js
@@ -7,37 +7,34 @@ class Tab {
 		this.className = className;
 		this.id = id;
 	}
-	createA() { // generate a element with tab data
+
+	createA() { // create a link element displaying content based on the provided Tab
 		var tablink = document.createElement('a');
 		tablink.innerHTML = this.name;
 		tablink.href = this.link;
-		tablink.id = this.id;
-		var sPage = window.location.pathname;
-		var sPath = sPage.substring(sPage.lastIndexOf('/')+1,sPage.length);
+
+		var sPage = window.location.pathname; // returns the user's current page
+		var sPath = sPage.substring(sPage.lastIndexOf('/')+1,sPage.length); // returns the individual filepath
 		tablink.className = this.className;
-		if(this.link == sPath) { 
-			tablink.id = "selectedtab";
-		}
-		else {
-			tablink.id = this.id;
-		}
+		if(this.link == sPath) { tablink.id = "selectedtab"; } // gives the link the id #selectedtab if the user is on the same page as the link's address 
+		else { tablink.id = this.id; }
+
 		return tablink;
 	}
 }
 
 function appendTabs(tablist) { // return div containing elements generated from tablist that function as tabs
 	var container = document.createElement('div'); // create div "container"
-	
+	container.id = "tabCTN";
 	for (var i = 0; i < (tablist.length); i++) {
-			link = tablist[i].createA();
-			container.appendChild(link);
+		var siddiv = document.createElement('div');
+		siddiv.className = "tabdiv";
+		link = tablist[i].createA();
+		siddiv.appendChild(link);
+		container.appendChild(siddiv);
 		}
 	return container;
 }
-
-var sidsTablist =	[new Tab("Home","index.html","tab",null),
-					 new Tab("Resume","resume.html","tab",null),
-					 new Tab("Portfolio ▾","portfolio.html","tab",null),];
 
 var header = document.createElement('div'); // create header
 header.id = "header";
@@ -45,9 +42,9 @@ header.id = "header";
 	var title = new Tab("Siddartha Thota","index.html","title",null);
 	header.appendChild(title.createA());
 
-	var tabCTN = document.createElement('div'); // create tab container
-	tabCTN.id = "tabCTN";
-		tabCTN.appendChild(appendTabs(sidsTablist));
-	header.appendChild(tabCTN);
+	var sidsTablist =	[new Tab("Home","index.html","tab",null),
+						 new Tab("Portfolio","portfolio.html","tab portfolio",null),
+						 new Tab("Resume","resume.html","tab",null),];
+	header.appendChild(appendTabs(sidsTablist)); // create and append tab container
 
 document.body.appendChild(header); // append header to body
